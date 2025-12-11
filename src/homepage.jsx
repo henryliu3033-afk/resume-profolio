fa101d1aa881ac66f9e62003669728097ae53514
henryliu3033-afk <henryliu3033@gmail.com>
2025-12-11 09:22:42 +0800

    update

--- src/homepage.jsx
+++ src/homepage.jsx
@@ -7,11 +7,13 @@ function HomePage() {
       welcomeTitle: "歡迎來到我的作品集",
       description: "這是虛擬的ps5網站",
       clickHere: "點擊這裡",
+      fakeIrent: "這是虛擬的租車網站",
     },
     en: {
       welcomeTitle: "Welcome to my work-profolio",
       description: "This is a virtual PS5 website",
       clickHere: "Clickhere",
+      fakeIrent: "This is fake renting car website",
     },
   };
   return (
@@ -33,7 +35,15 @@ function HomePage() {
               {texts[lang].clickHere}
             </a>
           </h2>
-          <h2></h2>
+          <h2 className="text-2xl flex gap-4  ">
+            {texts[lang].fakeIrent}
+            <a
+              href="https://rent-car-project-gpq3.vercel.app//"
+              className="text-purple-900 hover:text-green-700 "
+            >
+              {texts[lang].clickHere}
+            </a>
+          </h2>
         </div>
       </div>
     </>
